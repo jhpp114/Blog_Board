a7d0c5c2124dbdff9f68ff1dba5936570e741769
jhpp114 <jhpp114@uw.edu>
2020-06-29 17:34:55 +0900

    edit and delete is finished.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const { urlencoded } = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const PORT_NUMBER = 3000;
 const app = express();
 const Soccer = require('./models/soccer');
@@ -14,11 +15,13 @@ mongoose.connect('mongodb://localhost/my_blog_board', {
 })
 .then(() => console.log("DB Connected"))
 .catch( (error) => console.log(`Error on connecting database ${error}`));
+mongoose.set('useFindAndModify', false);
 
 // app configure
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // ================
 // ===ROUTE========
@@ -54,12 +57,28 @@ app.post('/blog/soccer', async function(req, res) {
     // dummyDataSoccer.push(newPostData);
     res.redirect('/blog/soccer');
 });
-
-
 // get detail page and render it base on the pass id.
 app.get('/blog/soccer/:id', async function(req, res) {
     let foundData = await Soccer.findById(req.params.id);
     res.render('soccer/detail', {foundData: foundData});
+});
+// render edit form to user base on the data found
+app.get('/blog/soccer/:id/edit', async function(req, res) {
+    let editData = await Soccer.findById(req.params.id);
+    res.render('soccer/edit', {editData: editData});
+});
+// submit editted data and save it into database
+app.put('/blog/soccer/:id', async function(req, res) {
+    let editSubmittedData = req.body.soccer;
+    await Soccer.findByIdAndUpdate(req.params.id, editSubmittedData);
+    res.redirect('/blog/soccer/' + req.params.id);
+});
+// delete the data
+app.delete('/blog/soccer/:id/', async function(req, res) {
+    let deleteItemId = req.params.id;
+    await Soccer.findByIdAndRemove(deleteItemId);
+    res.redirect('/blog/soccer/');
+    // so the data got deleted i guess
 });
 
 // Listen to the port
