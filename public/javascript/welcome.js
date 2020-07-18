97542bf29a4d3ba7e1eed0d4076cf10c8942fe21
jhpp114 <jhpp114@uw.edu>
2020-07-18 01:28:23 -0700

    added script for building img slider

---
+++ public/javascript/welcome.js
@@ -0,0 +1,31 @@
+// objects
+const slideData = [
+    {
+        img: "../images/Soccer_main.jpg"
+    ,   description: "The Soccer Blog"
+    }
+    ,  
+    {
+        img: "../images/Travel_main.jpg"
+    ,   description: "The Travel Blog"
+    }
+    ,
+    {
+        img: "../images/Programming_main.jpg"
+    ,   description: "The Programming Blog"
+    }
+    ,
+    {
+        img: "../images/Nature_main.jpg"
+    ,   description: "The Nature Blog"
+    }
+    ,
+    {
+        img: "../images/Coffee_main.jpg"
+    ,   description: "The Coffee Blog"
+    }
+];
+
+const slide_area = document.querySelector('.slide_show');
+console.log(slide_area);
+alert("Hello");
