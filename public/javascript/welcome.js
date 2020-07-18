46fae84a13c900a70c5109e0b9ecbc039a3c88ef
jhpp114 <jhpp114@uw.edu>
2020-07-18 03:09:01 -0700

    working on slider

--- public/javascript/welcome.js
+++ public/javascript/welcome.js
@@ -1,31 +1,45 @@
 // objects
-const slideData = [
-    {
-        img: "../images/Soccer_main.jpg"
-    ,   description: "The Soccer Blog"
-    }
-    ,  
-    {
-        img: "../images/Travel_main.jpg"
-    ,   description: "The Travel Blog"
-    }
-    ,
-    {
-        img: "../images/Programming_main.jpg"
-    ,   description: "The Programming Blog"
-    }
-    ,
-    {
-        img: "../images/Nature_main.jpg"
-    ,   description: "The Nature Blog"
-    }
-    ,
-    {
-        img: "../images/Coffee_main.jpg"
-    ,   description: "The Coffee Blog"
-    }
-];
+function getSlideData() {
+    const slideData = [ 
+        {
+            img: "../images/Travel_main.jpg"
+        ,   description: "The Travel Blog"
+        }
+        ,
+        {
+            img: "../images/Programming_main.jpg"
+        ,   description: "The Programming Blog"
+        }
+        ,
+        {
+            img: "../images/Nature_main.jpg"
+        ,   description: "The Nature Blog"
+        }
+        ,
+        {
+            img: "../images/Coffee_main.jpg"
+        ,   description: "The Coffee Blog"
+        }
+        ,
+        {
+            img: "../images/Soccer_main.jpg"
+        ,   description: "The Soccer Blog"
+        }
+    ];
+    return slideData;
+}
+
 
 const slide_area = document.querySelector('.slide_show');
-console.log(slide_area);
-alert("Hello");
+const left_slide_btn = document.querySelector('.fa-arrow-left');
+const right_slide_btn = document.querySelector('.fa-arrow-right');
+
+let counter = 0;
+function right_slide() {
+    let slideImgData = getSlideData();
+    slide_area.children[1].src = slideImgData[counter].img;
+    counter++;
+}
+console.log(slide_area.children[1].src);
+
+right_slide_btn.addEventListener("click", right_slide);
