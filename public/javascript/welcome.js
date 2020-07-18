9a3729c06c6c07346e52d008488cc3f32b833a72
jhpp114 <jhpp114@uw.edu>
2020-07-18 13:37:46 -0700

    left slider done

--- public/javascript/welcome.js
+++ public/javascript/welcome.js
@@ -37,15 +37,32 @@ const right_slide_btn = document.querySelector('.fa-arrow-right');
 let counter = 0;
 function right_slide() {
     let slideImgData = getSlideData();
-    if (!(counter >= slideImgData.length))
-    slide_area.children[1].src = slideImgData[counter].img;
-    counter++;
+    if (counter == slideImgData.length) {
+        counter = 0;
+        slide_area.children[1].src = slideImgData[counter].img;
+        slide_area.children[4].textContent = slideImgData[counter].description;
+        counter++;
+    } else {
+        slide_area.children[1].src = slideImgData[counter].img;
+        slide_area.children[4].textContent = slideImgData[counter].description;
+        counter++;
+    }
 }
 
 function left_slide() {
+    let sliderImageData = getSlideData();
+    if (counter == 0) {
+        counter = sliderImageData.length - 1;
+        console.log(counter);
+        slide_area.children[1].src = sliderImageData[counter].img;
+        slide_area.children[4].textContent = sliderImageData[counter].description;
+    } else {
+        slide_area.children[1].src = sliderImageData[--counter].img;
+        slide_area.children[4].textContent = sliderImageData[counter].description;
+    }
     
 }
 
-console.log(slide_area.children[1].src);
-
 right_slide_btn.addEventListener("click", right_slide);
+left_slide_btn.addEventListener("click", left_slide);
+console.log(slide_area.children);
