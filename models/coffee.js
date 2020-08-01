cafc6b701b085103628ed1fe354ddc0c584bda5c
jhpp114 <jhpp114@uw.edu>
2020-08-01 16:36:57 -0700

    prepare coffee blog

---
+++ models/coffee.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const coffeeSchema = new mongoose.Schema({
+    coffeename: String
+,   image: String
+,   description: String
+,   author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId
+        ,   ref: "User"
+        }
+        ,
+        username: String
+    }
+,   comments = [{
+        type: mongoose.Schema.Types.ObjectId
+    ,   ref: "Comment"
+    }]
+    ,
+    createdDate: {
+        type: Date
+    ,   default: Date.now
+    }
+});
+
+module.exports = new mongoose.model('Coffee', coffeeSchema);
